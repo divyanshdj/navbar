71453147a027500348a936324716bb656b2e8618
Divyansh Jain <124493395+divyanshdj@users.noreply.github.com>
2024-04-06 19:06:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+    const side = document.querySelector(".sidebar");
+    side.style.display = "flex";
+}
+
+function hideSideBar(){
+    const side = document.querySelector(".sidebar");
+    side.style.display = "none";
+}
