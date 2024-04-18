d1960d0f57366e8c131e69a80c10d04603e5aa0d
Divyansh Jain <124493395+divyanshdj@users.noreply.github.com>
2024-04-18 20:44:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSideBar(){
-    const side = document.querySelector(".sidebar");
-    side.style.display = "flex";
-}
-
-function hideSideBar(){
-    const side = document.querySelector(".sidebar");
-    side.style.display = "none";
-}
